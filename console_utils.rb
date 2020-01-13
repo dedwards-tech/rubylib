5caceafe7044323c7bb6cf6ea2795d430e10271e
dedwards-tech <dedwards.tech@gmail.com>
2020-01-13 00:40:59 -0800

    add cli helper module and class

--- console_utils.rb
+++ console_utils.rb
@@ -1,3 +1,6 @@
+require 'optparse'
+require 'ostruct'
+
 module ConsoleUtils
   def set_verbose(value=true)
     @verbose = value
@@ -13,5 +16,80 @@ module ConsoleUtils
 
   def is_dryrun
     @dryrun ||= false
+  end
+end
+
+
+# Design goals:
+#  * use object to directly reference argument vars
+#  * make block initializers optional for most simple arguments
+#
+
+module CliOptionBase
+  def args_list
+    @args_list ||= []
+  end
+
+  def arg_parser
+    @arg_parser ||= OptionParser.new
+  end
+
+  def reject(arg_opt)
+    if arg_opt.fetch(:value, nil)
+      raise OptionParser::InvalidArgument, "invalid value #{arg_opt[:value]} for #{arg_opt[:params[0]]}"
+      #raise ArgumentError("invalid value #{arg_opt[:value]} for #{arg_opt[:params[0]]}")
+    end
+  end
+
+  def set_required(arg_opt, req=true)
+    arg_opt[:required] = req
+  end
+
+  def set_value(arg_opt, value=nil)
+    arg_opt[:value] = value
+  end
+
+  def get_value(arg_opt)
+    arg_opt[:value]
+  end
+
+  def get_name(arg_opt)
+    arg_opt[:var_name]
+  end
+
+  def add_arg(var_name, init_value, long_arg, help_str)
+    new_arg = { :var_name => var_name, :value => init_value, :params => [ long_arg, help_str ] }
+    args_list.push(new_arg)
+    new_arg
+  end
+
+  def get_args(argv=nil)
+    if argv.nil?
+      arg_parser.parse!
+    else
+      arg_parser.parse!(argv)
+    end
+    os_vars = {}
+    args_list.each {|arg_opt| os_vars.merge!( { arg_opt[:var_name] => arg_opt[:value] } ) }
+    OpenStruct.new(os_vars)
+  end
+end
+
+class CliHelper
+  include CliOptionBase
+  attr_accessor :parser
+
+  def initialize
+    args_list
+    arg_parser
+  end
+
+  def new_simple(var_name, init_value, long_arg, help_str, &block)
+    this_arg = add_arg(var_name, init_value, long_arg, help_str, &block)
+    arg_parser.on(long_arg, help_str) do |o|
+      set_value(this_arg, o)
+      yield(this_arg) if block_given?
+    end
+    this_arg
   end
 end
--- unit-test/test_console_utils.rb
+++ unit-test/test_console_utils.rb
@@ -1,7 +1,59 @@
 require 'test/unit'
+require 'optparse'
 require_relative File.join('..', 'console_utils')
 
 class UnitTest_ConsoleUtils < Test::Unit::TestCase
-  def test_case1
+  def test_1_cli_helper_simple
+    my_cli = CliHelper.new
+    my_cli.new_simple('t_var1', 'tvar1_value', '--tvar1 [STR]',
+                      'Test1-tvar1 Unit Test var')
+    argv = ['--tvar1', 'argv_value1']
+    my_args = my_cli.get_args(argv)
+    assert_equal('argv_value1', my_args.t_var1, 'ERR: t_var1 arg value mismatch')
+  end
+
+  def test_2_cli_helper_with_block
+    my_cli = CliHelper.new
+    my_cli.new_simple('t_var2', 'tvar2_value', '--tvar2 [STR]',
+                      'Test2-tvar2 Unit Test var') do |opt_arg|
+      assert_equal('argv_value2', my_cli.get_value(opt_arg), 'ERR: t_var2 arg value mismatch')
+      my_cli.set_value(opt_arg, 'argv_value3')
+    end
+    argv = ['--tvar2', 'argv_value2']
+    my_args = my_cli.get_args(argv)
+    assert_equal('argv_value3', my_args.t_var2, 'ERR: t_var2 arg value block set mismatch')
+  end
+
+  def test_3_cli_helper_with_reject
+    my_cli = CliHelper.new
+    my_cli.new_simple('t_var3', 'tvar3_value', '--tvar3 [STR]',
+                      'Test3-tvar3 Unit Test var') do |opt_arg|
+      value = my_cli.get_value(opt_arg)
+      assert_equal('argv_value3', value, 'ERR: t_var3 arg value mismatch')
+      my_cli.reject(opt_arg)
+    end
+    argv = ['--tvar3', 'argv_value3']
+    assert_raise(OptionParser::InvalidArgument,
+                 "ERR: reject() should have raised Invalid Argument exception.") do
+      my_args = my_cli.get_args(argv)
+      assert_equal('argv_value3', my_args.t_var3, 'ERR: t_var3 arg value block set mismatch')
+    end
+  end
+
+  def test_4_cli_helper_no_args
+    my_cli = CliHelper.new
+    my_cli.new_simple('t_var4', 'tvar4_value', '--tvar4 [STR]',
+                      'Test4-tvar4 Unit Test var')
+    argv = []
+    my_args = my_cli.get_args(argv)
+    assert_not_nil(my_args, 'ERR: arguments should be empty')
+  end
+
+  def test_5_cli_helper_invalid_arg
+    my_cli = CliHelper.new
+    my_cli.new_simple('t_var5', 'tvar5_value', '--tvar5 [STR]',
+                      'Test5-tvar5 Unit Test var')
+    argv = ['--no-arg', 'testing123']
+    assert_raise(OptionParser::InvalidOption, "ERR: should have thrown invalid option exception for invalid arg") { my_cli.get_args(argv) }
   end
 end
